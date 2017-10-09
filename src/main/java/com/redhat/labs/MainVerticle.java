c7c2c8240e8242fb5043cb0eb449ead34b7acc6a
Deven Phillips <deven.phillips@redhat.com>
2017-10-09 12:44:19 -0400

    Initial implementation

--- src/main/java/com/redhat/labs/MainVerticle.java
+++ src/main/java/com/redhat/labs/MainVerticle.java
@@ -1,12 +1,21 @@
 package com.redhat.labs;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+import java.util.UUID;
 
 public class MainVerticle extends AbstractVerticle {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+    private static final String uuid = UUID.randomUUID().toString();
+
     @Override
     public void start() {
+        vertx.eventBus().publish("cluster.event", uuid);
 
+        vertx.eventBus().consumer("cluster.event", LOG::warn);
     }
 
 }
